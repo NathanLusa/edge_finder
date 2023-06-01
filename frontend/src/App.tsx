31fc1666a8c7a36c165a7f0a04081b0ea964b62b
Nathan Marcos Lusa <nathanlusa@iMac-de-Nathan.local>
2023-06-01 15:59:34 -0300

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,72 @@
-import { useEffect, useState } from 'react'
-import { SiteSchema } from './schemas'
-import { getVeiculos } from "./services"
-import Site from './components/Site'
+// import { useEffect, useState } from 'react'
+// import { SiteSchema } from './schemas'
+// import { getVeiculos } from "./services"
+// import Site from './components_old/Site'
 
 function App() {
-  const [sites, useSite] = useState<SiteSchema[]>([])
+    // const [sites, useSite] = useState<SiteSchema[]>([])
 
-  useEffect(() => { GetVeiculosAxios() }, [])
+    // useEffect(() => { GetVeiculosAxios() }, [])
 
-  const GetVeiculosAxios = async () => {
-    const sites = await getVeiculos()
-    useSite(sites)
-  }
+    // const GetVeiculosAxios = async () => {
+    //   const sites = await getVeiculos()
+    //   useSite(sites)
+    // }
 
-  return (
-    <div className="container p-5">
-      <h1>Edge finder</h1>
+    const range = Array.from(Array(5).keys()).map((x) => x + 1);
 
-      <div id="filter">
-        <button id="btn-teste">TESTE</button>
-      </div>
+    return (
+        <div className="container row-auto flex-row h-screen border">
+            {/* TITLE */}
+            <h1 className="text-center bg-blue-500">Edge finder</h1>
 
-      <div className="accordion accordion-flush">
-        {/* {sites.map((site, key) => Site(site, key))} */}
-        {/* {sites.map((site, key) => <Site key={key} id={site.id} nome={site.nome} veiculos={site.veiculos} />)} */}
-        {sites.map((site, key) => <Site key={key} {...site} />)}
-      </div>
+            {/* MAIN */}
+            <main className="container flex-row h-screen pt-2 bg-slate-200">
+                {range.map((_, key) => (
+                    <div key={key}>
+                        {/* <h5 className="bg-success border" key={key}>Teste {value}</h5> */}
 
-    </div>
-  )
+                        <div className="container bg-yellow-300 mb-3 border">
+                            <div className="grid grid-flow-col auto-cols-fr bg-blue-300">
+                                <div className=" bg-green-400">
+                                    <svg
+                                        className="d-block"
+                                        // width="400" height="200"
+                                        // xmlns="http://www.w3.org/2000/svg"
+                                        // role="img"
+                                        // aria-label="Placeholder: First slide"
+                                        // preserveAspectRatio="xMidYMid slice"
+                                        // focusable="false"
+                                    >
+                                        <rect
+                                            width="100%"
+                                            height="100%"
+                                            fill="#777"
+                                        ></rect>
+                                        <text
+                                            x="50%"
+                                            y="50%"
+                                            fill="#555"
+                                            dy=".3em"
+                                        ></text>
+                                    </svg>
+                                </div>
+                                <div className=" bg-blue-700">
+                                    <p>teste</p>
+                                </div>
+                            </div>
+
+                            <div className="grid grid-flow-col bg-red-400">
+                                <p className="">Ano: 2022</p>
+                                <p className="">KM: 186000</p>
+                                <p className="">Pato Branco - PR</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </main>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ frontend/src/App_old.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import { SiteSchema } from './schemas'
+import { getVeiculos } from "./services"
+import Site from './components_old/Site'
+
+function App() {
+  const [sites, useSite] = useState<SiteSchema[]>([])
+
+  useEffect(() => { GetVeiculosAxios() }, [])
+
+  const GetVeiculosAxios = async () => {
+    const sites = await getVeiculos()
+    useSite(sites)
+  }
+
+  return (
+    <div className="container p-5">
+      <h1>Edge finder</h1>
+
+      <div id="filter">
+        <button id="btn-teste">TESTE</button>
+      </div>
+
+      <div className="accordion accordion-flush">
+        {/* {sites.map((site, key) => Site(site, key))} */}
+        {/* {sites.map((site, key) => <Site key={key} id={site.id} nome={site.nome} veiculos={site.veiculos} />)} */}
+        {sites.map((site, key) => <Site key={key} {...site} />)}
+      </div>
+
+    </div>
+  )
+}
+
+export default App
