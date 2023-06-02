594751cc024dc76007f6aaa74ff154bfbf02a1cb
Nathan Marcos Lusa <nathanlusa@iMac-de-Nathan.local>
2023-06-02 15:41:58 -0300

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,54 +13,101 @@ function App() {
     //   useSite(sites)
     // }
 
-    const range = Array.from(Array(5).keys()).map((x) => x + 1);
+    const veiculos = [
+        {
+            id: 1,
+            marca: "Ford",
+            modelo: "Edge",
+            ano: 2013,
+            url: "https://pr.olx.com.br/regiao-de-curitiba-e-paranagua/autos-e-pecas/carros-vans-e-utilitarios/edge-limited-awd-1107289912",
+            titulo: "Edge Limited AWD",
+            site: "https://www.olx.com.br",
+            status: "indisponivel",
+        },
+        {
+            id: 2,
+            marca: "Ford",
+            modelo: "Edge",
+            ano: 2013,
+            url: "https://pr.olx.com.br/regiao-de-curitiba-e-paranagua/autos-e-pecas/carros-vans-e-utilitarios/ford-edge-limited-3-5-v6-24v-awd-aut-1151311492",
+            titulo: "FORD EDGE LIMITED 3.5 V6 24V AWD Aut.",
+            site: "https://www.olx.com.br",
+            status: "ativo",
+        },
+        {
+            id: 3,
+            marca: "Ford",
+            modelo: "Edge",
+            ano: 2013,
+            url: "https://pr.olx.com.br/regiao-de-curitiba-e-paranagua/autos-e-pecas/carros-vans-e-utilitarios/ford-edge-limited-3-5-v6-24v-awd-aut-2013-1174652575",
+            titulo: "FORD EDGE LIMITED 3.5 V6 24V AWD AUT 2013",
+            site: "https://www.olx.com.br",
+            status: "indisponivel",
+        },
+        {
+            id: 4,
+            marca: "Ford",
+            modelo: "Edge",
+            ano: 2013,
+            url: "https://pr.olx.com.br/regiao-de-curitiba-e-paranagua/autos-e-pecas/carros-vans-e-utilitarios/ford-edge-revisada-segundo-dono-1171372088",
+            titulo: "Ford Edge Revisada Segundo Dono",
+            site: "https://www.olx.com.br",
+            status: "indisponivel",
+        },
+        {
+            id: 5,
+            marca: "Ford",
+            modelo: "Edge",
+            ano: 2012,
+            url: "https://pr.olx.com.br/regiao-de-curitiba-e-paranagua/autos-e-pecas/carros-vans-e-utilitarios/ford-edege-limited-2012-awd-1180872331",
+            titulo: "Ford Edege Limited 2012 AWD",
+            site: "https://www.olx.com.br",
+            status: "ativo",
+        },
+    ];
 
     return (
-        <div className="container row-auto flex-row h-screen border">
+        <div className="container row-auto flex-row h-screen bg-sky-800 border">
             {/* TITLE */}
             <h1 className="text-center bg-blue-500">Edge finder</h1>
 
             {/* MAIN */}
-            <main className="container flex-row h-screen pt-2 bg-slate-200">
-                {range.map((_, key) => (
-                    <div key={key}>
-                        {/* <h5 className="bg-success border" key={key}>Teste {value}</h5> */}
+            <main className="grid grid-flow-row grid-cols-1 md:grid-cols-2 pt-2 gap-4 px-4 h-screen justify-center bg-red-600">
+                {veiculos.map((veiculo, key) => (
+                    <div
+                        key={key}
+                        className="flex-col rounded-lg max-w-md px-1 bg-yellow-300"
+                    >
+                        <div className="h-40 bg-green-400">
+                            <p>Imagem</p>
+                        </div>
+                        <div className="col-span-2 h-14 text-slate-100 bg-blue-400">
+                            <a href={veiculo.url}>
+                                {veiculo.marca} {veiculo.modelo}{" "}
+                                {veiculo.status}
+                            </a>
+                            <p>Preço: 12.000</p>
+                        </div>
 
-                        <div className="container bg-yellow-300 mb-3 border">
-                            <div className="grid grid-flow-col auto-cols-fr bg-blue-300">
-                                <div className=" bg-green-400">
-                                    <svg
-                                        className="d-block"
-                                        // width="400" height="200"
-                                        // xmlns="http://www.w3.org/2000/svg"
-                                        // role="img"
-                                        // aria-label="Placeholder: First slide"
-                                        // preserveAspectRatio="xMidYMid slice"
-                                        // focusable="false"
-                                    >
-                                        <rect
-                                            width="100%"
-                                            height="100%"
-                                            fill="#777"
-                                        ></rect>
-                                        <text
-                                            x="50%"
-                                            y="50%"
-                                            fill="#555"
-                                            dy=".3em"
-                                        ></text>
-                                    </svg>
-                                </div>
-                                <div className=" bg-blue-700">
-                                    <p>teste</p>
-                                </div>
-                            </div>
-
-                            <div className="grid grid-flow-col bg-red-400">
-                                <p className="">Ano: 2022</p>
-                                <p className="">KM: 186000</p>
-                                <p className="">Pato Branco - PR</p>
-                            </div>
+                        <div className="grid grid-flow-col justify-between items-center h-10 px-4 bg-red-400">
+                            <h6>
+                                <span className="badge">
+                                    <i className="fa-solid fa-sack-dollar"></i>
+                                </span>{" "}
+                                {veiculo.ano}
+                            </h6>
+                            <h6>
+                                <span className="badge">
+                                    <i className="fa-regular fa-gauge"></i>
+                                </span>{" "}
+                                12000
+                            </h6>
+                            <h6>
+                                <span className="badge">
+                                    <i className="fa-solid fa-clock"></i>
+                                </span>{" "}
+                                Pato Branco - PR
+                            </h6>
                         </div>
                     </div>
                 ))}
