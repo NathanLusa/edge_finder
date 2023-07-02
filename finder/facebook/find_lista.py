6f92825fe688449d939c73dec2bce449ae7a51d8
Nathan Marcos Lusa <nathanlusa@iMac-de-Nathan.local>
2023-07-02 18:39:21 -0300

    .

--- finder/facebook/find_lista.py
+++ finder/facebook/find_lista.py
@@ -443,10 +443,8 @@ def find_facebook(force):
                 #     veiculo.add_historico(historico_json)
 
         # sel = Selenium(scroll_times=scroll_times)
-        # veiculos_ativos = [x for x in veiculos if x.status == 'ativo']
-        veiculos_ativos = [
-            x for x in veiculos if x.status == 'ativo' and x.id >= 1195
-        ]
+        veiculos_ativos = [x for x in veiculos if x.status == 'ativo']
+        veiculos_ativos = [x for x in veiculos if x.status == 'ativo' and x.id >= 1210]
         for veiculo in veiculos_ativos:
             sel.scroll_times = 0
             item = sel.scrape_facebook_marketplace_item(
